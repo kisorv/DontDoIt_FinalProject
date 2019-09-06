1cb77253ea37720d44eee699a91da10cbfd9d09d
Veronica <kisorv@gmail.com>
2019-09-06 16:35:52 -0400

    added routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,20 @@ import { WeddingComponent } from "./wedding/wedding.component";
 import { FirstDateComponent } from "./first-date/first-date.component";
 import { InterviewsComponent } from "./interviews/interviews.component";
 import { TopicsComponent } from "./topics/topics.component";
+import { HomeComponent } from "./home/home.component";
+import { RouterModule, Routes } from "@angular/router";
+import { DontdoitService } from "./dontdoit.service";
+import { QuizComponent } from "./quiz/quiz.component";
+
+const appRoutes: Routes = [
+  { path: "home", component: HomeComponent },
+  { path: "wedding", component: WeddingComponent },
+  { path: "interview", component: InterviewsComponent },
+  { path: "firstdate", component: FirstDateComponent },
+  { path: "topics", component: TopicsComponent },
+  { path: "quiz", component: QuizComponent },
+  { path: "", redirectTo: "/home", pathMatch: "full" }
+];
 
 @NgModule({
   declarations: [
@@ -13,10 +27,12 @@ import { TopicsComponent } from "./topics/topics.component";
     WeddingComponent,
     FirstDateComponent,
     InterviewsComponent,
-    TopicsComponent
+    TopicsComponent,
+    HomeComponent,
+    QuizComponent
   ],
-  imports: [BrowserModule, HttpClientModule],
-  providers: [],
+  imports: [BrowserModule, HttpClientModule, RouterModule.forRoot(appRoutes)],
+  providers: [DontdoitService],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
