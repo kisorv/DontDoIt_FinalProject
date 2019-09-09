9859468ab6a322a735047db6ebe57991ff077dee
Veronica <kisorv@gmail.com>
2019-09-09 11:06:47 -0400

    first quiz component is displaying questions/answers

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const weddingRoute = require("./routes/weddingRoute.api");
+const driversRoute = require("./routes/driversQuiz.api");
 
 app.use(express.json());
 app.use(cors());
 app.use("/", weddingRoute);
+app.use("/", driversRoute);
 
 const port = 8080;
 app.listen(port, () => console.log(`Server is listening on port: ${port}.`));
--- src/app/quiz/quiz.component.ts
+++ src/app/quiz/quiz.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+// import { NgForm } from "@angular/forms";
+import { DontdoitService } from "../dontdoit.service";
 
 @Component({
-  selector: 'app-quiz',
-  templateUrl: './quiz.component.html',
-  styleUrls: ['./quiz.component.css']
+  selector: "app-quiz",
+  templateUrl: "./quiz.component.html",
+  styleUrls: ["./quiz.component.css"]
 })
 export class QuizComponent implements OnInit {
+  questions: any;
 
-  constructor() { }
+  constructor(private dontdoitService: DontdoitService) {}
 
   ngOnInit() {
+    this.dontdoitService.getDriversQuestions().subscribe(response => {
+      this.questions = response;
+    });
   }
-
 }
