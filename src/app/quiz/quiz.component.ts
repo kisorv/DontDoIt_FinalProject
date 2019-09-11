52e58500d85fc3ce15f9fa1755b427aa83990730
Sugene Mulpuri <sugene@Sugenes-MacBook-Pro-2.local>
2019-09-11 15:19:57 -0400

    more giphy stickers etc

--- src/app/quiz/quiz.component.ts
+++ src/app/quiz/quiz.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
+import { DontdoitService } from '../dontdoit.service';
+import { delay } from 'q';
 
 @Component({
   selector: "app-quiz",
@@ -7,11 +9,41 @@ import { Router } from "@angular/router";
   styleUrls: ["./quiz.component.css"]
 })
 export class QuizComponent implements OnInit {
-  constructor() {}
+  loop: number = 1;
+  index: number = Math.floor((Math.random() * 500));
+  delay: number = 10000;
+  stickerData: any;
 
-  ngOnInit() {}
+  constructor(private dontDoitService: DontdoitService) { }
 
-  // navigateToDriversQuiz() {
-  //   this.router.navigate(["drivers"]);
-  // }
-}
+
+  indexFlow() {
+    
+    if (this.index === 500) {
+      this.index -= 500;
+      console.log(this.index);
+  
+    }
+    else {
+  
+    
+    this.index++;
+    console.log(this.index);
+    }
+  }
+
+  ngOnInit() {
+    if (this.loop === 1) {
+      // setTimeout(this.indexFlow, this.delay);
+      setInterval(this.indexFlow.bind(this), 10000);
+   
+      this.dontDoitService.getStickerData().subscribe(response => {
+        console.log(response.data);
+        
+        this.stickerData = response.data;
+        
+        console.log(this.stickerData);
+      });
+  }
+
+}}
